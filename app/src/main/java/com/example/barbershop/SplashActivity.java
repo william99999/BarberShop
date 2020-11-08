16ba65315c5167f0212b334d7f405c27e8f8253f
Abhishek Kumar <sethi.abhi16.as@gmail.com>
2020-11-08 04:15:10 -0800

    Changes done to splash

--- app/src/main/java/com/example/barbershop/SplashActivity.java
+++ app/src/main/java/com/example/barbershop/SplashActivity.java
@@ -2,7 +2,11 @@ package com.example.barbershop;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class SplashActivity extends AppCompatActivity {
 
@@ -10,5 +14,18 @@ public class SplashActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        TimerTask timerTask=new TimerTask() {
+            @Override
+            public void run() {
+                finish();
+                startActivity(new Intent(SplashActivity.this,
+                        MainActivity.class));
+
+
+            }
+        };
+        Timer timer=new Timer();
+        timer.schedule(timerTask,1000);
     }
 }
